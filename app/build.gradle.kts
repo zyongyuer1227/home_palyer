354931171c374bdd61ecdec7b15cb0bc54e9d45e
zyongyuer <zyongyuer@163.com>
2026-08-24 21:38:44 +0800

    build: bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.iptv.player"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     if (hasReleaseSigning) {
